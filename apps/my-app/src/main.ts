8833e701b5b81343e042b4ab73b84beafad3d95d
Nicholas Cunningham <ndcunningham@gmail.com>
2023-04-19 09:50:39 -0600

    adding cookie-parser

--- apps/my-app/src/main.ts
+++ apps/my-app/src/main.ts
@@ -6,10 +6,14 @@
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 
+import cookieParser from "cookie-parser";
 import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+  
   const globalPrefix = "api";
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
